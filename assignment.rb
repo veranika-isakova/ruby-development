9ba47aea3fb7f4f72598d7573e1035229fa4adf3
VERANIKA <veranika.isakova89@gmail.com>
2018-02-17 16:46:12 +0100

    Get output2: change instance-type and allow-ssh-from

--- assignment.rb
+++ assignment.rb
@@ -9,6 +9,27 @@ require './properties_instance.rb'
 require './security.rb'
 require './instance.rb'
 require './securitygroup'
+require 'optparse'
+
+#instances, instance-type, allow-ssh-from = ARGV
+#args = Hash[ ARGV.flat_map{|s| s.scan(/--?([^=\s]+)(?:=(\S+))?/) } ]
+#number_of_instances =
+options = {}
+options[:instances] = 1,
+options[:instanceType] = "t2.micro",
+options[:allowsshfrom] = "0.0.0.0/0"
+
+OptionParser.new do |parser|
+  parser.on("--instance-type TYPE", "Type of the instance") do |v|
+    options[:instanceType] = v
+  end
+  parser.on("--instances NUMBER", "Number of the instances") do |v|
+    options[:instances] = v
+  end
+  parser.on("--allow-ssh-from TYPE", "Allow ssh from") do |v|
+    options[:allowsshfrom] = v
+  end
+end.parse!
 
 template = Template.new
 outputs = Outputs.new
@@ -29,18 +50,20 @@ publicIP.Description = "Public IP address of the newly created EC2 instance"
 publicIP.Value = value
 value.Fn = ["EC2Instance", "PublicIp"]
 template.Resources = resources
+
 resources.EC2Instance = ec2instance
 ec2instance.Properties = properties_ec2Instance
 properties_ec2Instance.ImageId = "ami-b97a12ce"
-properties_ec2Instance.InstanceType = "t2.micro"
+properties_ec2Instance.InstanceType = options[:instanceType]
 properties_ec2Instance.SecurityGroups = [securityGroup]
 securityGroup.Ref = "InstanceSecurityGroup"
 ec2instance.Type = "AWS::EC2::Instance"
+
 resources.InstanceSecurityGroup = instanceSecurityGroup
 instanceSecurityGroup.Properties = properties_instanceSecurityGroup
 properties_instanceSecurityGroup.GroupDescription = "Enable SSH access via port 22"
 properties_instanceSecurityGroup.SecurityGroupIngress = [securityGroupIngress]
-securityGroupIngress.CidrIp = "0.0.0.0/0"
+securityGroupIngress.CidrIp = options[:allowsshfrom]
 securityGroupIngress.FromPort = "22"
 securityGroupIngress.IpProtocol = "tcp"
 securityGroupIngress.ToPort = "22"
@@ -48,3 +71,6 @@ instanceSecurityGroup.Type = "AWS::EC2::SecurityGroup"
 
 
 puts JSON.pretty_generate(template)
+
+
+#puts ARGV
