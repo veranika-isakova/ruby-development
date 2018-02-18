e54b163d7b611b8d86c5cfd8fa631d60fe2d7c7f
VERANIKA <veranika.isakova89@gmail.com>
2018-02-18 16:39:11 +0100

    Pass the test of the second output

--- spec/output.rb
+++ spec/output.rb
@@ -1,11 +1,11 @@
 require_relative '../template_generator.rb'
 
-RSpec.describe TemplateGenerator  do
+RSpec.describe TemplateGenerator do
   options = {}
   options[:instances] = "1"
   options[:instanceType] = "t2.micro"
   options[:allowsshfrom] = "0.0.0.0/0"
-  it "should has this output" do
+  it "should have the first output" do
     expect(TemplateGenerator::generate(options)).to eq({
       "AWSTemplateFormatVersion": "2010-09-09",
       "Outputs": {
@@ -43,5 +43,59 @@ RSpec.describe TemplateGenerator  do
       "Type": "AWS::EC2::SecurityGroup"
     } }
     })
+  end
+
+it "should have the second output" do
+  options[:instances] = "2"
+  options[:instanceType] = "t2.small"
+  options[:allowsshfrom] = "37.17.210.74/32"
+    expect(TemplateGenerator::generate(options)).to eq({
+        "AWSTemplateFormatVersion": "2010-09-09",
+        "Outputs": {
+          "PublicIP": {
+            "Description": "Public IP address of the newly created EC2 instance",
+            "Value": {
+              "Fn::GetAtt": [
+                "EC2Instance",
+                "PublicIp"
+      ] }
+      } },
+        "Resources": {
+          "EC2Instance": {
+            "Properties": {
+              "ImageId": "ami-b97a12ce",
+              "InstanceType": "t2.small",
+              "SecurityGroups": [
+                {
+                  "Ref": "InstanceSecurityGroup"
+      } ]
+      },
+            "Type": "AWS::EC2::Instance"
+          },
+          "EC2Instance2": {
+           "Properties": {
+          "ImageId": "ami-b97a12ce",
+          "InstanceType": "t2.small",
+          "SecurityGroups": [
+            {
+              "Ref": "InstanceSecurityGroup"
+      } ]
+      },
+        "Type": "AWS::EC2::Instance"
+      },
+      "InstanceSecurityGroup": {
+        "Properties": {
+          "GroupDescription": "Enable SSH access via port 22",
+          "SecurityGroupIngress": [
+            {
+              "CidrIp": "37.17.210.74/32",
+              "FromPort": "22",
+              "IpProtocol": "tcp",
+              "ToPort": "22"
+      } ]
+        },
+        "Type": "AWS::EC2::SecurityGroup"
+      } }
+      })
   end
 end
